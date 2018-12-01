d94c613b7cffed1ec49a044b7d3efd145c0c5d67
Wither <wither.tweets@gmail.com>
2018-12-02 00:58:47 +0100

    Added window deinstancing

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 
 let win
 
-function createWindow () {
-  win = new BrowserWindow({
-  })
+function createWindow() {
+    win = new BrowserWindow()
 
-  win.setMenu(null)
-  win.maximize()
-  win.loadFile('index.html')
+    win.setMenu(null)
+    win.maximize()
+    win.loadFile('index.html')
+
+    win.on('closed', () => {
+        win = null
+    })
 }
 
 app.on('ready', createWindow)
