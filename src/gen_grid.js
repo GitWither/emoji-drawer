9a1119e1751f0b186fb721e9ccc59d88eb2a50f3
Wither <wither.tweets@gmail.com>
2018-12-02 02:30:20 +0100

    Added comments and changed column variable name

--- src/gen_grid.js
+++ src/gen_grid.js
@@ -1,20 +1,32 @@
 function genGrid(width, height) {
+    //Get the drawer div
     var drawer = document.getElementById('drawer');
+    //Create the table element
     var table = document.createElement('table');
     for(var x = 0; x < width; x++) {
+        //Create table row elements
         var tableRow = document.createElement('tr')
+        //Set their id
         tableRow.id = `r${x}`
         for(var y = 0; y < height; y++) {
-            var tableH = document.createElement('th');
-            tableH.id = `r${x}c${y}`;
+            //Create table column elements
+            var tableColumn = document.createElement('th');
+            //Set their id
+            tableColumn.id = `r${x}c${y}`;
 
+            //Create dropdown elements
             var select = document.createElement('select');
+            //Set their id
             select.id = `r${x}c${y}`;
 
-            tableH.appendChild(select);
+            //Append dropdown elements to columns
+            tableColumn.appendChild(select);
+            //Append columns to rows
             tableRow.appendChild(tableH);
         }
+        //Append rows to table
         table.appendChild(tableRow);
     }
+    //Append table to drawer div
     drawer.appendChild(table);
 }
